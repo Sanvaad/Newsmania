03db0cb5cd040b97909e1beb741429d030e85e78
Sanvaad <96122726+Sanvaad@users.noreply.github.com>
2024-07-03 14:53:29 +0530

    Add files via upload

---
+++ src/features/auth/Logout.tsx
@@ -0,0 +1,21 @@
+import { HiArrowRightOnRectangle } from "react-icons/hi2";
+import ButtonIcon from "../../ui/ButtonIcon";
+import { useLogout } from "./useLogout";
+import logoutIcon from "../../images/logout.svg";
+import Spinner from "../../ui/Spinner";
+
+function Logout() {
+  const { logout, isLoading } = useLogout();
+
+  return (
+    <button disabled={isLoading} onClick={logout}>
+      {!isLoading ? (
+        <img src={logoutIcon} alt="" className="h-8" />
+      ) : (
+        <Spinner />
+      )}
+    </button>
+  );
+}
+
+export default Logout;
